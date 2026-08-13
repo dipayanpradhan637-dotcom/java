e06b2ea5fa72334bed13b8d70f455b7cfa1a88c9
Dipanjan Pradhan <dipayanpradhan637@gmail.com>
2026-08-13 19:06:06 +0530

    Initial commit

---
+++ StarPattern.java
@@ -0,0 +1,147 @@
+public class StarPattern {
+    public static void main(String[] args)
+     {
+
+        for (int i = 1; i <= 5; i++) 
+        {
+            for (int j = 1; j <= i; j++) 
+            {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+        System.out.println(" ");
+        System.out.println(" ");
+          for(int i = 1; i <= 5; i++)
+         {
+            for(int j = 1; j <= 5; j++) 
+            {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+        System.out.println(" ");
+        System.out.println(" ");
+        for(int i = 5; i >= 1; i--)
+         {
+              for(int j = 1; j <= i; j++) 
+              {
+                System.out.print("* ");
+              }
+                System.out.println();
+         }
+             System.out.println(" ");
+            System.out.println(" ");
+            for(int i = 1; i <= 5; i++) {
+
+    for(int j = 1; j <= 5 - i; j++) {
+        System.out.print(" ");
+    }
+
+    for(int j = 1; j <= 2 * i - 1; j++) 
+    {
+        System.out.print("*");
+    }
+
+       System.out.println();
+    }
+        System.out.println(" ");
+        System.out.println(" ");
+        for(int i = 5; i >= 1; i--) {
+
+    for(int j = 1; j <= 5 - i; j++) {
+        System.out.print(" ");
+    }
+
+    for(int j = 1; j <= 2 * i - 1; j++) {
+        System.out.print("*");
+    }
+
+    System.out.println();
+}
+System.out.println(" ");
+        System.out.println(" ");
+        for(int i = 1; i <= 5; i++) {
+
+    for(int j = 1; j <= 5 - i; j++)
+        System.out.print(" ");
+
+    for(int j = 1; j <= 2 * i - 1; j++)
+        System.out.print("*");
+
+    System.out.println();
+}
+
+for(int i = 4; i >= 1; i--) {
+
+    for(int j = 1; j <= 5 - i; j++)
+        System.out.print(" ");
+
+    for(int j = 1; j <= 2 * i - 1; j++)
+        System.out.print("*");
+
+    System.out.println();
+}
+System.out.println(" ");
+        System.out.println(" ");
+        for(int i = 1; i <= 5; i++) {
+
+    for(int j = 1; j <= 5; j++) {
+
+        if(i == 1 || i == 5 || j == 1 || j == 5)
+            System.out.print("* ");
+        else
+            System.out.print("  ");
+    }
+
+    System.out.println();
+}
+        System.out.println(" ");
+        System.out.println(" ");
+        for(int i = 1; i <= 5; i++) {
+
+    for(int j = 1; j <= i; j++) {
+
+        if(j == 1 || j == i || i == 5)
+            System.out.print("* ");
+        else
+            System.out.print("  ");
+    }
+
+    System.out.println();
+}
+System.out.println(" ");
+        System.out.println(" ");
+        for(int i = 1; i <= 5; i++) {
+
+    for(int j = 1; j <= 5; j++) {
+
+        if(j == i || j == 6 - i)
+            System.out.print("* ");
+        else
+            System.out.print("  ");
+    }
+
+    System.out.println();
+}
+System.out.println(" ");
+        System.out.println(" ");
+        for(int i = 1; i <= 5; i++) {
+
+    for(int j = 1; j <= 5; j++) {
+
+        if(i == 3 || j == 3)
+            System.out.print("* ");
+        else
+            System.out.print("  ");
+    }
+
+    System.out.println();
+}
+System.out.println(" ");
+        System.out.println(" ");
+    }
+    
+}
+
+   
